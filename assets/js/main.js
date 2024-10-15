a3aff4d598ede555a30630822bdacf6d275e4235
Pourya Zaeemfar <pzaeemfar@gmail.com>
2024-10-15 06:32:28 +0330

    Replace image gallery with a text element

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,15 @@
-async function loadMedia() {
-  const gistUrl =
-    "https://api.github.com/gists/7ea01c8c359712d0a38aeebdadffad3c";
-  const response = await fetch(gistUrl);
-  const gist = await response.json();
-  const links = getLinksFromGist(gist);
+const url =
+  "https://gist.githubusercontent.com/pzaeemfar/16c59c223ea5a1efdcbbd3fe9a57fbef/raw/nomercy.ir.txt";
 
-  const mediaGrid = document.querySelector(".gallery");
-  links.forEach((link) => addMediaToGrid(mediaGrid, link));
+async function loadContent() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const text = await response.text();
+    document.getElementById("text").innerHTML = text.replace(/\n/g, "<br>");
+  } catch (error) {
+    console.error("Error loading the content:", error);
+  }
 }
 
-function getLinksFromGist(gist) {
-  const content = gist.files["nomercy.ir.txt"].content;
-  return content.split("\n").filter((link) => link.trim() !== "");
-}
-
-function addMediaToGrid(mediaGrid, link) {
-  const div = document.createElement("div");
-  const img = createImageElement(link);
-
-  div.appendChild(img);
-  mediaGrid.appendChild(div);
-}
-
-function createImageElement(link) {
-  const img = document.createElement("img");
-  img.src = link;
-  img.className = "h-auto max-w-full rounded-lg";
-  return img;
-}
-
-loadMedia();
+loadContent();
