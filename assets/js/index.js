7b47e850a361b0db6a42ab20170c50281fa11fd9
Pourya Zaeemfar <pzaeemfar@gmail.com>
2025-02-15 13:08:21 +0330

    Add static files

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+const urls = [
+  "https://soundcloud.com/bahramnouraei/khoob",
+  "https://soundcloud.com/alisorena/badhaye-vahshi-by-farshad",
+  "https://soundcloud.com/bahramnouraei/lams",
+];
+
+const tracks = document.querySelector("#tracks");
+urls.forEach((url) => {
+  const item = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = url;
+  link.target = "_blank";
+  link.textContent = url.split("/").pop();
+  item.appendChild(link);
+
+  tracks.appendChild(item);
+});
+
+const footerHeading = document.querySelector(".footer-heading");
+
+const DATE = new Date("2025-02-15");
+const currentDate = new Date();
+const timeDifference = currentDate - DATE;
+const daysPassed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+footerHeading.innerHTML = `FUCK LIFE, DAY <b>#${daysPassed}</b>`;
